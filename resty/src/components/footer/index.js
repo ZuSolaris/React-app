da00b09f352b5f3481e9ca429ffe41dbb6d93de8
Zoe Gonzalez <zusolaris.personal@gmail.com>
2023-01-03 00:40:02 +0000

    configured and reformatted lab from class to functional components

---
+++ resty/src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./footer.scss" 
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+function Footer(){
+  return <footer>&copy; 2023</footer>;
+}
+
+export default Footer;
